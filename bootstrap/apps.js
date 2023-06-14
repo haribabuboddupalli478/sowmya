7c1cef6804f4aac6feedae65abd457dfdb4e9aa0
haribabuboddupalli478 <133846524+haribabuboddupalli478@users.noreply.github.com>
2023-06-14 08:49:25 +0530

    12-6-2023

---
+++ bootstrap/apps.js
@@ -0,0 +1,30 @@
+ {
+    var x=20;
+}
+console.log(x);
+{
+    let x=40;
+    console.log(x);
+}
+{
+    const first_name="web";
+    window.prompt(first_name);
+}
+{
+    const x=20;
+    const y=30;
+    console.log(x);
+}
+{
+    const button=document.querySelector("button");
+    button.addEventListener("click",
+    console.log("buttonclick")
+     );
+ }
+
+ {
+    let x=3020;
+    let y=140;
+    let z=x*y;
+    console.log(z);
+ }
