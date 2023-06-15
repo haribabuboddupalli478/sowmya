2cb558f3e0ae8775e7d77d5ceafe580d4495144b
haribabuboddupalli478 <133846524+haribabuboddupalli478@users.noreply.github.com>
2023-06-15 12:07:09 +0530

    15-6-2023

--- bootstrap/apps.js
+++ bootstrap/apps.js
@@ -1,30 +1,120 @@
- {
-    var x=20;
-}
-console.log(x);
-{
-    let x=40;
-    console.log(x);
-}
-{
-    const first_name="web";
-    window.prompt(first_name);
-}
-{
-    const x=20;
-    const y=30;
-    console.log(x);
-}
-{
-    const button=document.querySelector("button");
-    button.addEventListener("click",
-    console.log("buttonclick")
-     );
- }
+//  {
+//     var x=20;
+// }
+// console.log(x);
+// {
+//     let x=40;
+//     console.log(x);
+// }
+// {
+//     const first_name="web";
+//     window.prompt(first_name);
+// }
+// {
+//     const x=20;
+//     const y=30;
+//     console.log(x);
+// }
+// {
+//     const button=document.querySelector("button");
+//     button.addEventListener("click",
+//     console.log("buttonclick")
+//      );
+//  }
 
- {
-    let x=3020;
-    let y=140;
-    let z=x*y;
-    console.log(z);
- }
+
+
+//  {
+//     let x=3020;
+//     let y=140;
+//     let z=x*y;
+//     console.log(z);
+//  }
+
+
+//  {
+// const x="30";
+// const y="40";
+// console.log(x+y);
+//  }
+
+
+// function web(x,y){
+//     return console.log(x+y);
+// }
+// web(3,4)
+
+
+
+// let x=30;
+// let y="justify";
+// let z="40";
+// let q=x-y;
+// console.log(q);
+
+
+
+// const x="50";
+// const y="50";
+// console.log(x==y);
+
+
+
+// const x=50;
+// const y="50";
+// console.log(x===y);
+
+
+
+// function dm(x,y){
+//     return console.log(x**y);
+// };
+// dm(30,40);
+
+
+
+// function webdevelopment(){
+//     x=30;
+//     y=40;
+// };
+// webdevelopment();
+// console.log(x);
+// console.log(y);
+
+
+// var x=40;
+// console.log(typeof(x));
+
+
+// var x="40";
+// console.log(typeof(x));
+
+
+// let x;
+// console.log(typeof(x));
+
+
+// const obj={name:"web",age:20,designation:"developer"};
+// console.log(obj);
+
+// const obj={name:"web",age:20,designation:"developer"};
+// console.log(obj.name);
+
+// const obj={name:"web",age:20,designation:"developer"};
+// console.log(obj.age);
+
+// const obj={name:"web",age:20,designation:"developer"};
+// console.log(obj.designation);
+
+
+// const clickBtn=document.querySelector(".clickbtn");
+// const headTag=document.querySelector(".hidehead");
+// clickBtn.addEventListener("click",function web(){
+//     return headTag.classList.toggle("hidehead-active");
+// });
+
+
+// const headTag=document.querySelector(".hidehead");
+// function web(){
+//     return headTag.classList.toggle("hidehead-active");
+// };
